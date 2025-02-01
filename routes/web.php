197d5e6056c1f9dab02ec8b90723bbf529112158
taro419 <oblbnvsb1520lfoub@docomo.ne.jp>
2025-02-01 22:39:43 +0900

    publicでログイン画面が表示されるように

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('login');
+});
 // Route::get('/home', 'HomeController@index')->name('home');
 
 //Auth::routes();
